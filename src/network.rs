f8eb627d3474d22fd040923d82fa6b732b030aef
razor389 <rgranowski@gmail.com>
2024-09-10 10:45:15 -0500

    pass stage

--- src/network.rs
+++ src/network.rs
@@ -119,7 +119,7 @@ fn handle_client(
                         
                         wait_command_active = true; // Set WAIT command state to active
                     }
-                } else if command == Some("REPLCONF".to_string()) && args[0].contains("ACK") {
+                } else if command == Some("REPLCONF".to_string()) && args[0].to_uppercase()=="ACK" {
                     if wait_command_active {
                         responding_slaves += 1; // Count the ACK
 
